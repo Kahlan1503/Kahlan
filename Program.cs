62993d2d7acd3265db216a26348014c3542bdaf0
teri888@inbox.ru <teri888@inbox.ru>
2022-11-08 19:50:37 +0300

    Program.cs

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿int MAX_WORD_LENGTH = 3;
+string[] array1 = { "Hello", "2", "world", ":-)" };
+string[] array2 = { "1234", "1567", "-2", "computer science" };
+string[] array3 = { "Russia", "Denmark", "Kazan" };
+
+string[] GetShortWordsArray(string[] array)
+{
+    int length = array.Length;
+    string[] result = new string[length];
+    int count = 0;
+
+    for (int i = 0; i < length; i++)
+    {
+        if (array[i].Length <= MAX_WORD_LENGTH)
+        {
+            result[count] = array[i];
+            count++;
+        }
+    }
+    Array.Resize(ref result, count);
+
+    return result;
+}
+
+void PrintArray(string[] array)
+{
+    if (array.Length == 0) // Проверяем на пустоту массив
+    {
+        Console.WriteLine("[]");
+    }
+    else
+    {
+        Console.Write("[");
+        for (int i = 0; i < array.Length - 1; i++)
+        {
+            Console.Write($"\"{array[i]}\", ");
+        }
+        Console.Write($"\"{array[array.Length - 1]}\"");
+        Console.WriteLine("]");
+    }
+}
+
+void PrintTask(string[] array)
+{
+    Console.WriteLine("Исходный массив:");
+    PrintArray(array);
+    string[] shortArray = GetShortWordsArray(array);
+    Console.WriteLine($"Массив результирующий (строка меньше или равна {MAX_WORD_LENGTH} символам):");
+    PrintArray(shortArray);
+    Console.WriteLine();
+}
+
+PrintTask(array1);
+PrintTask(array2);
+PrintTask(array3);
